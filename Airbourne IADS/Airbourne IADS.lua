b2a4e402a2b4186b3193bebaeb05a2b7bdaff1f2
Sacha Ligthert <sacha@ligthert.net>
2020-04-08 02:05:57 +0200

    The "OMFG I should be in bed" update.

--- Airbourne IADS/Airbourne IADS.lua
+++ Airbourne IADS/Airbourne IADS.lua
@@ -1,15 +1,16 @@
 -- Setup the A2A dispatcher, and initialize it.
-A2ADispatcher = AI_A2A_GCICAP:NewWithBorder( { "RU EWR" }, { "RU Sqn" }, "RU Border", { "CAP Zone" }, 2, 15000, 50000, 50000, 100000, 24 )
+A2ADispatcher = AI_A2A_GCICAP:NewWithBorder( { "RU EWR" }, { "RU Sqn" }, "RU Border", { "CAP Zone" }, 10, 15000, 60000, 150000, 30 )
 
 -- A2ADispatcher:SetTacticalDisplay( true )
 
 -- Dispatcher settings
-A2ADispatcher:SetDefaultTakeoffFromParkingCold()
+-- A2ADispatcher:SetDefaultTakeoffFromParkingCold()
+A2ADispatcher:SetSquadronTakeoffFromRunway()
 A2ADispatcher:SetDefaultLandingAtEngineShutdown()
 A2ADispatcher:SetDefaultFuelThreshold(0.20)
-A2ADispatcher:SetIntercept(300)
+A2ADispatcher:SetIntercept(10)
 A2ADispatcher:SetDefaultDamageThreshold(0.95)
-A2ADispatcher:SetOverhead(0.6)
+A2ADispatcher:SetOverhead(0.8)
 
 -- Kick off the Dispatcher
 A2ADispatcher:Start()
