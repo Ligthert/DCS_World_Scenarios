342feac18ba44b480cb7e9f8d7eff2e0b922436a
Sacha Ligthert <sacha@ligthert.net>
2020-04-14 00:11:47 +0200

    - Limited civie traffic to REDFOR - Starting populating the hostile airbase

--- Environmental Hazard/Environmental Hazard.lua
+++ Environmental Hazard/Environmental Hazard.lua
@@ -1,33 +1,37 @@
 ------ Random Air Traffic
 ---- BLUFOR
 -- Spawn AN-26B
-local us_rat_1 =  RAT:New("US RAT #001")
-us_rat_1:SetTakeoff("cold")
-us_rat_1:ContinueJourney()
-us_rat_1:ExcludedAirports({"Senaki-Kolkhi","Krasnodar-Center"})
-us_rat_1:Spawn(5)
+-- local us_rat_1 =  RAT:New("US RAT #001")
+-- us_rat_1:SetTakeoff("cold")
+-- us_rat_1:ContinueJourney()
+-- us_rat_1:ExcludedAirports("Krasnodar-Center")
+-- us_rat_1:Invisible()
+-- us_rat_1:Spawn(5)
 
 -- Spawn C-17A
-local us_rat_2 =  RAT:New("US RAT #002")
-us_rat_2:SetTakeoff("cold")
-us_rat_2:SetTerminalType(AIRBASE.TerminalType.OpenBig)
-us_rat_2:ContinueJourney()
-us_rat_2:ExcludedAirports({"Senaki-Kolkhi","Krasnodar-Center"})
-us_rat_2:Spawn(5)
+-- local us_rat_2 =  RAT:New("US RAT #002")
+-- us_rat_2:SetTakeoff("cold")
+-- us_rat_2:SetTerminalType(AIRBASE.TerminalType.OpenBig)
+-- us_rat_2:ContinueJourney()
+-- us_rat_2:ExcludedAirports("Krasnodar-Center")
+-- us_rat_2:Invisible()
+-- us_rat_2:Spawn(5)
 
 -- Spawn C-130
-local us_rat_3 =  RAT:New("US RAT #003")
-us_rat_3:SetTakeoff("cold")
-us_rat_3:ContinueJourney()
-us_rat_3:ExcludedAirports({"Senaki-Kolkhi","Krasnodar-Center"})
-us_rat_3:Spawn(5)
+-- local us_rat_3 =  RAT:New("US RAT #003")
+-- us_rat_3:SetTakeoff("cold")
+-- us_rat_3:ContinueJourney()
+-- us_rat_3:ExcludedAirports("Krasnodar-Center")
+-- us_rat_3:Invisible()
+-- us_rat_3:Spawn(5)
 
 -- Spawn some shitty biplane
-local us_rat_4 =  RAT:New("US RAT #004")
-us_rat_4:SetTakeoff("cold")
-us_rat_4:ContinueJourney()
-us_rat_4:ExcludedAirports({"Senaki-Kolkhi","Krasnodar-Center"})
-us_rat_4:Spawn(5)
+-- local us_rat_4 =  RAT:New("US RAT #004")
+-- us_rat_4:SetTakeoff("cold")
+-- us_rat_4:ContinueJourney()
+-- us_rat_4:ExcludedAirports("Krasnodar-Center")
+-- us_rat_4:Invisible()
+-- us_rat_4:Spawn(5)
 
 
 ---- REDFOR
@@ -35,21 +39,30 @@ us_rat_4:Spawn(5)
 local ru_rat_1 = RAT:New("RU RAT #001")
 ru_rat_1:SetTakeoff("cold")
 ru_rat_1:ContinueJourney()
-ru_rat_1:ExcludedAirports({"Senaki-Kolkhi","Krasnodar-Center"})
+ru_rat_1:ExcludedAirports("Krasnodar-Center")
+ru_rat_1:Invisible()
+ru_rat_1:SetCoalition("sameonly")
+ru_rat_1:ATC_Messages(false)
 ru_rat_1:Spawn(5)
 
 -- An-30M
 local ru_rat_2 = RAT:New("RU RAT #002")
 ru_rat_2:SetTakeoff("cold")
 ru_rat_2:ContinueJourney()
-ru_rat_2:ExcludedAirports({"Senaki-Kolkhi","Krasnodar-Center"})
+ru_rat_2:ExcludedAirports("Krasnodar-Center")
+ru_rat_2:Invisible()
+ru_rat_2:SetCoalition("sameonly")
+ru_rat_2:ATC_Messages(false)
 ru_rat_2:Spawn(5)
 
 -- Spawn some shitty novelty item
 local ru_rat_3 = RAT:New("RU RAT #003")
 ru_rat_3:SetTakeoff("cold")
 ru_rat_3:ContinueJourney()
-ru_rat_3:ExcludedAirports({"Senaki-Kolkhi","Krasnodar-Center"})
+ru_rat_3:ExcludedAirports("Krasnodar-Center")
+ru_rat_3:Invisible()
+ru_rat_3:SetCoalition("sameonly")
+ru_rat_3:ATC_Messages(false)
 ru_rat_3:Spawn(5)
 
 -- Spawn IL-76MD
@@ -57,15 +70,20 @@ local ru_rat_4 = RAT:New("RU RAT #004")
 ru_rat_4:SetTakeoff("cold")
 ru_rat_4:SetTerminalType(AIRBASE.TerminalType.OpenBig)
 ru_rat_4:ContinueJourney()
-ru_rat_4:ExcludedAirports({"Senaki-Kolkhi","Krasnodar-Center"})
+ru_rat_4:ExcludedAirports("Krasnodar-Center")
+ru_rat_4:Invisible()
+ru_rat_4:SetCoalition("sameonly")
+ru_rat_4:ATC_Messages(false)
 ru_rat_4:Spawn(5)
 
 -- Spawn Yak-40
 local ru_rat_5 = RAT:New("RU RAT #005")
 ru_rat_5:SetTakeoff("cold")
 ru_rat_5:ContinueJourney()
-ru_rat_5:ExcludedAirports({"Senaki-Kolkhi","Krasnodar-Center"})
+ru_rat_5:ExcludedAirports("Krasnodar-Center")
+ru_rat_5:Invisible()
+ru_rat_5:SetCoalition("sameonly")
+ru_rat_5:ATC_Messages(false)
 ru_rat_5:Spawn(5)
-
 
 ------ Sead Spawn
