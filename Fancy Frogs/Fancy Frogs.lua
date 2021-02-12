07ca2bcdd2ea30155f8447b96f9b4210c451bac7
Sacha Ligthert <sacha@ligthert.net>
2021-02-12 19:00:53 +0100

    Final edit for Fancy Frogs (2021-02-12)

--- Fancy Frogs/Fancy Frogs.lua
+++ Fancy Frogs/Fancy Frogs.lua
@@ -6,11 +6,11 @@ A2ADispatcher = AI_A2A_GCICAP:NewWithBorder( { "RU EWR" }, { "RU Sqn" }, "RU Bor
 -- Dispatcher settings
 --A2ADispatcher:SetDefaultTakeoffFromParkingCold()
 A2ADispatcher:SetSquadronTakeoffFromRunway()
-A2ADispatcher:SetDefaultLandingAtEngineShutdown()
+A2ADispatcher:SetDefaultLandingAtRunway()
 A2ADispatcher:SetDefaultFuelThreshold(0.20)
-A2ADispatcher:SetIntercept(300)
+A2ADispatcher:SetIntercept(200)
 A2ADispatcher:SetDefaultDamageThreshold(0.95)
-A2ADispatcher:SetOverhead(1)
+A2ADispatcher:SetOverhead(0.66)
 
 -- Kick off the Dispatcher
 A2ADispatcher:Start()
