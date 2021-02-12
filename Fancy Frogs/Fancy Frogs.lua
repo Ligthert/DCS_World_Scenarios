64e65302924d274336e6d064f0abb2d689c61d98
Sacha Ligthert <sacha@ligthert.net>
2021-02-12 23:03:56 +0100

    Updates aan de Fancy Frogs scenario

--- Fancy Frogs/Fancy Frogs.lua
+++ Fancy Frogs/Fancy Frogs.lua
@@ -1,16 +1,15 @@
 -- Setup the A2A dispatcher, and initialize it.
-A2ADispatcher = AI_A2A_GCICAP:NewWithBorder( { "RU EWR" }, { "RU Sqn" }, "RU Border", { "CAP Zone" }, 8, 15000, 60000, 150000, 12 )
+A2ADispatcher = AI_A2A_GCICAP:NewWithBorder( { "RU EWR" }, { "RU Sqn" }, "RU Border", { "CAP Zone" }, 2, 15000, 60000, 150000, 4 )
 
 -- A2ADispatcher:SetTacticalDisplay( true )
 
 -- Dispatcher settings
---A2ADispatcher:SetDefaultTakeoffFromParkingCold()
 A2ADispatcher:SetSquadronTakeoffFromRunway()
 A2ADispatcher:SetDefaultLandingAtRunway()
 A2ADispatcher:SetDefaultFuelThreshold(0.20)
-A2ADispatcher:SetIntercept(200)
+A2ADispatcher:SetIntercept(600)
 A2ADispatcher:SetDefaultDamageThreshold(0.95)
-A2ADispatcher:SetOverhead(0.66)
+A2ADispatcher:SetOverhead(0.50)
 
 -- Kick off the Dispatcher
 A2ADispatcher:Start()
