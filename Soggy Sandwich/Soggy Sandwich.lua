9265cb4a882c345bb383f95b797427fca7f3914c
Sacha Ligthert <sacha@ligthert.net>
2021-02-12 19:01:27 +0100

    Almost finished Soggy Sandwhich

---
+++ Soggy Sandwich/Soggy Sandwich.lua
@@ -0,0 +1,20 @@
+patrol_khashuri1 = GROUP:FindByName( "patrol_khashuri1" )
+patrol_khashuri1:PatrolZones( { ZONE:New( "city_khashuri" ) }, 16, "Vee" )
+
+patrol_khashuri2 = GROUP:FindByName( "patrol_khashuri2" )
+patrol_khashuri2:PatrolZones( { ZONE:New( "city_khashuri" ) }, 16, "Vee" )
+
+patrol_khashuri3 = GROUP:FindByName( "patrol_khashuri3" )
+patrol_khashuri3:PatrolZones( { ZONE:New( "city_khashuri" ) }, 16, "Vee" )
+
+patrol_khashuri4 = GROUP:FindByName( "patrol_khashuri4" )
+patrol_khashuri4:PatrolZones( { ZONE:New( "city_khashuri" ) }, 16, "Vee" )
+
+patrol_khashuri5 = GROUP:FindByName( "patrol_khashuri5" )
+patrol_khashuri5:PatrolZones( { ZONE:New( "city_khashuri" ) }, 16, "Vee" )
+
+patrol_khashuri6 = GROUP:FindByName( "patrol_khashuri6" )
+patrol_khashuri6:PatrolZones( { ZONE:New( "city_khashuri" ) }, 16, "Vee" )
+
+vic_khashuri = GROUP:FindByName( "vic_khashuri" )
+vic_khashuri:PatrolZones( { ZONE:New( "city_khashuri" ) }, 16, "Vee" )
---
+++ Soggy Sandwich/notes.md
@@ -0,0 +1,11 @@
+Hawg1 and Boar2
+
+
+1. Fence In
+2. IP
+3. Clear city (Hawg1: Khashuri; Boar2: Oni )
+4. Clear supply stash (Hawg1: Kekhvi; Board2: Didi-Gupta)
+5. Exfil
+6. Fence Out
+7. Finals
+8. Land on Vaziani
