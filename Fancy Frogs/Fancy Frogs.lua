d00806dc4c3eacba1acddbd4613eef7c7ae10484
Sacha Ligthert <sacha@ligthert.net>
2021-04-09 17:37:21 +0200

    20210409 - Prep for the great night! :-)

--- Fancy Frogs/Fancy Frogs.lua
+++ Fancy Frogs/Fancy Frogs.lua
@@ -7,7 +7,7 @@ A2ADispatcher = AI_A2A_GCICAP:NewWithBorder( { "RU EWR" }, { "RU Sqn" }, "RU Bor
 A2ADispatcher:SetSquadronTakeoffFromRunway()
 A2ADispatcher:SetDefaultLandingAtRunway()
 A2ADispatcher:SetDefaultFuelThreshold(0.20)
-A2ADispatcher:SetIntercept(600)
+A2ADispatcher:SetIntercept(900)
 A2ADispatcher:SetDefaultDamageThreshold(0.95)
 A2ADispatcher:SetOverhead(0.50)
 
