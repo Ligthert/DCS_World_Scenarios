28a4e158cce23918ec6050a9f0951f8d7f010433
Sacha Ligthert <sacha@ligthert.net>
2024-03-06 23:39:31 +0100

    Update to latest version.

--- Airbourne IADS/Airbourne IADS.lua
+++ Airbourne IADS/Airbourne IADS.lua
@@ -1,5 +1,5 @@
 -- Setup the A2A dispatcher, and initialize it.
-A2ADispatcher = AI_A2A_GCICAP:NewWithBorder( { "RU EWR" }, { "RU Sqn" }, "RU Border", { "CAP Zone" }, 2, 15000, 60000, 150000, 4 )
+A2ADispatcher = AI_A2A_GCICAP:NewWithBorder( { "RU EWR" }, { "RU Sqn" }, "RU Border", { "CAP Zone" }, 2, 15000, 60000, 150000, 8 )
 
 -- A2ADispatcher:SetTacticalDisplay( true )
 
