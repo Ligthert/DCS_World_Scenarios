be32c79d9f0ad1f86967862bae8b77ba10d999f3
Sacha Ligthert <sacha@ligthert.net>
2024-03-08 00:19:49 +0100

    Initial Setup for Operation Hotcake.

---
+++ Operation Hotcake/Operation Hotcake.lua
@@ -0,0 +1,16 @@
+-- Setup the A2A dispatcher, and initialize it.
+A2ADispatcher = AI_A2A_GCICAP:NewWithBorder( { "RU EWR" }, { "RU Sqn" }, "RU Border", { "CAP Zone" }, 2, 15000, 60000, 150000, 8 )
+
+-- A2ADispatcher:SetTacticalDisplay( true )
+
+-- Dispatcher settings
+A2ADispatcher:SetSquadronTakeoffFromRunway()
+A2ADispatcher:SetDefaultLandingAtRunway()
+A2ADispatcher:SetDefaultFuelThreshold(0.20)
+-- A2ADispatcher:SetIntercept(900)
+A2ADispatcher:SetDefaultDamageThreshold(0.95)
+A2ADispatcher:SetOverhead(0.50)
+A2ADispatcher:SetDefaultCapTimeInterval(150,300)
+
+-- Kick off the Dispatcher
+A2ADispatcher:Start()
